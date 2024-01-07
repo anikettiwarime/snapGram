4c3becc74a8d9a87ed4fd5f2031ab25a2504966f
Aniket Tiwari <anikettiwari1570@gmail.com>
2024-01-07 21:43:03 +0530

    Add user cards to AllUsers page

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,8 +1,22 @@
+import UserCard from "@/components/shared/UserCard";
+import { useGetUsers } from "@/lib/react-query/queriesAndMutation";
 
 const AllUsers = () => {
+  const { data: users } = useGetUsers();
   return (
-    <div>AllUsers</div>
-  )
-}
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        <ul className="user-grid">
+          {users?.documents.map((user) => (
+            <li key={user?.$id} className="flex-1 min-w-[200px] w-full">
+              <UserCard user={user} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default AllUsers
+export default AllUsers;
