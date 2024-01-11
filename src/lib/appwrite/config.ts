5507da0fd6389c62c2236386b304c9227727e656
Aniket Tiwari <anikettiwari1570@gmail.com>
2024-01-11 17:53:32 +0530

    Update Appwrite configuration

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,15 +1,15 @@
 import { Client, Databases, Account, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-  projectId: import.meta.env.VITE_APPWRITE_ID,
-  url: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
 
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
 
-  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
-  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-  saveCollectionId: import.meta.env.VITE_APPWRITE_SAVE_COLLECTION_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
+  saveCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 };
 
 export const client = new Client();
